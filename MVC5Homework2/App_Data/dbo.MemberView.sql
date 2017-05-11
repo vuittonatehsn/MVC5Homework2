beae0ec3d2b771891c49cc29acad49f65277706e
patrick.kuo_ehsn <patrick.kuo_ehsn@hotmail.com>
2017-05-11 22:51:36 +0800

    to submit  DB file you must open VS in administrator mode

--- MVC5Homework2/App_Data/dbo.MemberView.sql
+++ MVC5Homework2/App_Data/dbo.MemberView.sql
@@ -1,16 +1,12 @@
 ﻿CREATE VIEW dbo.MemberView
 AS
-SELECT DISTINCT q.客戶名稱,
-                             (SELECT        COUNT(a.Id) AS Expr1
-                               FROM            dbo.客戶資料 AS a INNER JOIN
-                                                         dbo.客戶聯絡人 AS b ON a.Id = b.客戶Id
-                               WHERE        (b.IsDeleted <> 1)) AS 聯絡人數量,
-                             (SELECT        COUNT(a.Id) AS Expr1
-                               FROM            dbo.客戶資料 AS a INNER JOIN
-                                                         dbo.客戶銀行資訊 AS c ON a.Id = c.客戶Id
-                               WHERE        (c.IsDeleted <> 1)) AS 銀行帳戶數量
-FROM            dbo.客戶資料 AS q INNER JOIN
-                         dbo.客戶聯絡人 AS b ON q.Id = b.客戶Id INNER JOIN
-                         dbo.客戶銀行資訊 AS c ON q.Id = c.客戶Id
-WHERE        (q.IsDeleted <> 1)
-GO
+
+select a.客戶名稱, (SELECT        COUNT(b.Id)
+					FROM           dbo.客戶聯絡人 b 
+					WHERE a.Id = b.客戶Id AND (b.IsDeleted <> 1) 
+					) AS 聯絡人數量
+				, (SELECT        COUNT(c.Id)
+					FROM            dbo.客戶銀行資訊 AS c 
+					WHERE a.Id = c.客戶Id AND (c.IsDeleted <> 1)
+					) AS 銀行帳戶數量
+FROM 客戶資料 a 
