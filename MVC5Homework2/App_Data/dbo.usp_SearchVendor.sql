ad7c332d9d156a58da7f9775015c25612af6f168
vuittonatehsn <vuittonatehsn@gmail.com>
2017-05-12 13:43:13 +0800

    include radio button and fix query at search procedure

--- MVC5Homework2/App_Data/dbo.usp_SearchVendor.sql
+++ MVC5Homework2/App_Data/dbo.usp_SearchVendor.sql
@@ -4,7 +4,8 @@ AS
 	SELECT a.Id, a.Email, a.地址, a.客戶名稱, a.統一編號, a.傳真, a.電話
 		,b.分行代碼, b.帳戶名稱, b.帳戶號碼, b.銀行名稱
 		,c.姓名, c.職稱, c.電話 AS 聯絡人電話, c.手機
-	FROM 客戶資料 a, 客戶銀行資訊 b, 客戶聯絡人 c
+	FROM 客戶資料 a INNER JOIN 客戶銀行資訊 b ON a.id = b.客戶Id 
+			INNER JOIN 客戶聯絡人 c ON a.id = c.客戶Id 
 	WHERE a.客戶名稱 like N'%'+@param1+'%' 
 	OR b.銀行名稱 like N'%'+@param1+'%' 
 	OR c.姓名 like N'%'+@param1+'%'
